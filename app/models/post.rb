747a70b6d704a54231e8be74cfea33711a241fbd
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-08-11 12:54:57 -0400

    commit tree f60151e6678ac85885ccaf065c72385a69a0b73c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait
+
+
+    def title_is_clickbait
+            unless title&.include?("Won't Believe" || "Secret" || "Top 10" || "Guess")
+                errors.add(:title, "Not clickbaity enough")
+            end
+    end
 end
